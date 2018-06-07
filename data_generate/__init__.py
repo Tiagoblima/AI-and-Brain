35cd38ff7a5f59a7e0ea5f4578fa82caf1f3a380
Tiagoblima <tiago.blima@ufrpe.br>
2018-06-06 22:27:52 -0300

    Alterações finais

--- data_generate/__init__.py
+++ data_generate/__init__.py
@@ -6,30 +6,62 @@ from typing import List
 def data_generate():
     ##Simulation with random numbers
 
-    features_train = [[1, 30], [1, 24],[1, 86],[1,219],[2,34],[2,63]]  # type:List[List[int]]
-    labels_train = [0,1,1,1,0,1]
-    # for i in range(4000):
-    #  x = randrange(0, 4)
-    #  y = randrange(0, 4)
-    #    coordnate = [x, y]
-    #   features_train.append(coordnate)
-    #    if (x == 1 & y == 3) | (x == 2 & y > 1) | (x == 3 & y > 1):
-    #       labels_train.append(1)
-    #    else:
-    #        labels_train.append(0)
+    features_train = [[1, 0.5]]  # type:
+    labels_train = [1]
 
-    features_test = [[3, 3],[2,62],[3,23],[3,19]]  # type: List[List[int]]
-    labels_test = [0,1,0,0]
+    for i in range(400):
 
-    #for i in range(2000):
-     #   x = randrange(0, 6)
-     #   y = randrange(0, 6)
-     #  coordnate = [x, y]
-     # features_test.append(coordnate)
-     #   if (x == 1 & y == 3) | (x == 2 & y > 1) | (x == 3 & y > 1):
-      #      labels_test.append(1)
-      #  else:
-       #     labels_test.append(0) "
+        x = randrange(0, 41).__float__()/10
+
+        qtd = randrange(10, 101)
+        hit = randrange(0, qtd + 1)
+        y = hit.__float__() / qtd.__float__()
+        features_train.append([x, y])
+
+        if x <= 1:
+            if y > 0.7:
+                labels_train.append(1)
+            else:
+                labels_train.append(0)
+        elif x <= 2.0:
+            if y > 0.6:
+                labels_train.append(1)
+            else:
+                labels_train.append(0)
+        elif x > 2.0:
+            if y > 0.4:
+                labels_train.append(1)
+            else:
+                labels_train.append(0)
 
 
-    return features_train, features_test, labels_train,labels_test
+    features_test = [[3, 0.9]]  # type: List[List[int]]
+    labels_test = [0]
+
+    for i in range(50):
+
+        x = randrange(1, 4)
+
+        qtd = randrange(10, 300)
+        hit = randrange(0, qtd + 1)
+        y = hit.__float__() / qtd.__float__()
+        features_test.append([x, y])
+
+        if x == 1:
+            if y > 0.7:
+                labels_test.append(1)
+            else:
+                labels_test.append(0)
+        elif x == 2:
+            if y > 0.6:
+                labels_test.append(1)
+            else:
+                labels_test.append(0)
+        elif x == 3:
+            if y > 0.5:
+                labels_test.append(1)
+            else:
+                labels_test.append(0)
+
+
+    return features_train, features_test, labels_train, labels_test
--- test.py
+++ test.py
@@ -3,34 +3,30 @@ from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
 from data_generate import data_generate
-from picture import prettyPicture
+
 
 features_train, features_test, labels_train, labels_test = data_generate()
-plt.xlim(0,5)
-plt.ylim(0,5)
+good_grade = [features_train[ii][1] for ii in range(0,len(features_train)) if labels_train[ii] == 1]
+high_level = [features_train[ii][0] for ii in range(0,len(features_train)) if labels_train[ii] == 1]
+bad_grade =  [features_train[ii][1] for ii in range(0,len(features_train)) if labels_train[ii] == 0]
+low_level =  [features_train[ii][0]  for ii in range(0,len(features_train)) if labels_train[ii] == 0]
 
-high_level = [features_train[ii][0] for ii in range(0,len(features_train))if labels_train==1]
-good_average = [features_train[ii][0] for ii in range(0,len(features_train))if labels_train==1]
-low_level = [features_train[ii][0] for ii in range(0,len(features_train))if labels_train==1]
-bad_avarege = [features_train[ii][0] for ii in range(0,len(features_train))if labels_train==1]
-plt.scatter(high_level,good_average,color="b",label="Good Average")
-plt.scatter(low_level,bad_avarege,color="r",label="Bad Avarege")
-plt.legend()
-plt.xlabel("Level")
-plt.ylabel("Averege")
-plt.show()
 
-clf = SVC()
+plt.xlim(1, 4)
+plt.ylim(0, 1)
+plt.ylabel('Percentege of hits')
+plt.xlabel('Level task')
+
+
+plt.scatter(high_level,good_grade,c='b')
+plt.scatter(low_level,bad_grade,c='r')
+plt.axis([1, 4,0,1])
+plt.show();
+
+clf = SVC(kernel="rbf")
 clf.fit(features_train,labels_train)
 
 pred = clf.predict(features_test)
 
-print pred
-
 acc = accuracy_score(labels_test,pred)
-print acc
-
-try:
-    prettyPicture(clf, features_test, labels_test)
-except NameError:
-    pass
+print "Accuracy: {0}".format(acc)
